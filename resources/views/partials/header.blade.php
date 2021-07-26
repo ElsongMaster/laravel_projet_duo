ac3f6431833c52c7b9b6e424298ec7a83450e50d
StanTheMan7 <sts.graur@gmail.com>
2021-07-26 14:38:41 +0200

    laravelisation page home et index

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+
+<header id="masthead" class="site-header" data-anchor-target=".hero" data-top="background: rgba(59,58,54,0); border-bottom-color: rgba(226,226,226,0);" data-top-bottom="background: rgba(59,58,54,1); border-bottom-color: rgba(226,226,226,1);">
+
+    @include('partials.nav')
+
+</header><!-- /#masthead -->
+
+<div class="hero background-overlay">
+    
+    <div class="hero-text">
+        <h1>Simplicity is the soul of efficiency</h1>
+    </div> <!-- /.hero-text -->
+    <div class="hero-arrow">
+        <a class="scrollTo" href="#"><i class="fa fa-angle-down" aria-hidden="true"></i></a>
+    </div><!-- /.hero-arrow -->
+
+</div><!-- /.hero -->
