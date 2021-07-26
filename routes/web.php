d34ddecbb244aef6f9ade5a9f84ba327a7d88dc3
ElsongMaster <elsongmaster@gmail.com>
2021-07-26 12:20:53 +0200

    Modif Erreur

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Htpp\Controllers\indexController;
+use App\Http\Controllers\indexController;
 
 /*
 |--------------------------------------------------------------------------
